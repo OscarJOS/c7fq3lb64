9b369e9078b1d72b0a6cd2664ee4e1d4962f23e3
Oscar F <oscarffu@gmail.com>
2018-09-25 19:18:30 -0500

    Filtros

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,13 @@
 class ExpensesController < ApplicationController
   def index
      @expenses = Expense.order("date DESC")
-  end
-end
+
+     if params[:concept].present?
+       @expenses = Expense.where("concept LIKE ?", "%#{params[:concept]}%")
+     end
+     if params[:category_id].present?
+       @expenses = Expense.where("category_id = ?", params[:category_id])
+     end
+
+   end
+ end
